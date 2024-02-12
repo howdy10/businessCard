9190614a9dbf3743a57c4b776bc2748def7149c0
Howdy <howdy1078@gmail.com>
2024-02-12 14:28:30 -0700

    Hold for new computer

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,39 @@
 "use client";
-import { Paper } from "@mui/material";
-import { Stack } from "@mui/material";
+import { Box, Divider, Paper, Typography, Stack } from "@mui/material";
+import Grid from "@mui/material/Unstable_Grid2";
+import { useTheme } from "@mui/material/styles";
+import userData from "../utils/data.json";
+import { useState } from "react";
+import EmailIcon from "@mui/icons-material/Email";
+import LocalPhoneIcon from "@mui/icons-material/LocalPhone";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import XIcon from "@mui/icons-material/X";
+import IconButton from "@mui/material/IconButton";
+import { styled } from "@mui/material/styles";
 
 //Helful things
 //SX sizing prop: https://mui.com/system/sizing/
+//CSS background color change https://developer.mozilla.org/en-US/docs/Web/CSS/gradient
+
+interface User {
+  email: string;
+  phone: string;
+  socialMedia: UserSocials;
+}
+interface UserSocials {
+  linkedIn: string;
+  twitter: string;
+  github: string;
+}
+
+const UserItem = styled(Typography)(({ theme }) => ({
+  color: "white",
+}));
 
 export default function Page() {
+  const theme = useTheme();
+  const [user, setUser] = useState<User | undefined>(userData);
   return (
     <Stack
       direction='row'
@@ -19,8 +47,48 @@ export default function Page() {
     >
       <Paper
         elevation={5}
-        sx={{ height: "50%", width: " 50%", borderRadius: 3 }}
-      ></Paper>
+        sx={{
+          height: "50%",
+          width: "50%",
+          borderRadius: 3,
+          background: `linear-gradient(to right, ${theme.palette.primary.main} 33%, white 33%);`,
+        }}
+      >
+        <Grid container spacing={2}>
+          <Grid xs={4}>
+            <Stack
+              direction='column'
+              justifyContent='flex-end'
+              alignItems='center'
+              spacing={2}
+            >
+              <UserItem>
+                <EmailIcon />
+                Email
+              </UserItem>
+              <UserItem>{user.email}</UserItem>
+              <UserItem>
+                <LocalPhoneIcon />
+                Phone
+              </UserItem>
+              <UserItem>{user.phone}</UserItem>
+              <Divider />
+              <UserItem>Find me on</UserItem>
+              <Box color={"white"}>
+                <IconButton color='inherit'>
+                  <GitHubIcon />
+                </IconButton>
+                <IconButton color='inherit'>
+                  <LinkedInIcon />
+                </IconButton>
+                <IconButton color='inherit'>
+                  <XIcon />
+                </IconButton>
+              </Box>
+            </Stack>
+          </Grid>
+        </Grid>
+      </Paper>
     </Stack>
   );
 }
--- utils/theme.ts
+++ utils/theme.ts
@@ -1,4 +1,21 @@
 import { createTheme } from "@mui/material/styles";
+declare module "@mui/material/styles" {
+  interface TypographyVariants {
+    userDetails: React.CSSProperties;
+  }
+
+  // allow configuration using `createTheme`
+  interface TypographyVariantsOptions {
+    userDetails?: React.CSSProperties;
+  }
+}
+
+// Update the Typography's variant prop options
+declare module "@mui/material/Typography" {
+  interface TypographyPropsVariantOverrides {
+    userDetails: true;
+  }
+}
 
 export const theme = createTheme({
   palette: {
@@ -8,6 +25,11 @@ export const theme = createTheme({
     primary: {
       main: "#2E7D32",
       light: "#C8E6C9",
+    },
+  },
+  typography: {
+    userDetails: {
+      color: "white",
     },
   },
 });
